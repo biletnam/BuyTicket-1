f7972fd70a13bf7f1387a6a77091fe0f161cfcf1
sureshdevaraj <suresh.k.devaraj@gmail.com>
2017-04-20 16:29:46 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var apiai = require('apiai');
+var apiai = require('./core/extlib/apiai/index.js');
 var express = require('express');
 var bodyParser = require('body-parser');
 var uuid = require('node-uuid');
@@ -19,8 +19,10 @@ var SEVER_IP_ADDR = process.env.OPENSHIFT_NODEJS_IP || process.env.HEROKU_IP || 
 var FB_VERIFY_TOKEN = config.FBVERIFYTOKEN;
 var APIAI_ACCESS_TOKEN = config.APIAIACCESSTOKEN;
 var APIAI_LANG = 'en';
-var APIAI_VERIFY_TOKEN = config.APIAI_VERIFY_TOKEN;
+var APIAI_VERIFY_TOKEN = 'verify123';
 var apiAiService = apiai(APIAI_ACCESS_TOKEN);
+var sessionIds = new Map();
+var xhr = new XMLHttpRequest();
 
 log4js.configure({
     appenders:
@@ -51,8 +53,355 @@ var ChatHistoryLog = log4js.getLogger('Historylog');
 var app = express();
 app.use(bodyParser.text({ type: 'application/json' }));
 
-app.listen(REST_PORT, SEVER_IP_ADDR, function () {
+app.listen(8081, SEVER_IP_ADDR, function () {
     logger.debug('Rest service ready on port ' + REST_PORT);
 });
 
+app.get('/apipolling/', function (req, res) {
+    logger.debug("Inside api polling");
+    try {
+        res.end("welcome");
+    }
+    catch (err) {
+        
+    }
 
+});
+
+function processEvent(event, userCoversationArr) {
+
+    logger.debug("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< START OF MESSAGE REQUEST >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+
+    var sender = event.sender.id.toString();
+    var inpustr = '';
+    var msgText = '';
+
+    if (event.message) {
+        msgText = event.message.text;
+    }
+
+    async.series({
+        one: function (callback) {
+            if (inpustr != '') {
+
+
+            }
+            else {
+
+            }
+
+        }
+    }, function (err, results) {
+
+        if ((event.message && event.message.text) || (event.postback && event.postback.payload)) {
+
+            var text = event.message ? event.message.text : event.postback.payload;
+
+            if (event.message && event.message.quick_reply && event.message.quick_reply.payload) text = event.message.quick_reply.payload;
+
+            logger.debug("Before Account Linking ");
+
+            if (!sessionIds.has(sender)) {
+                //logger.debug("Inside sessionID:- ");
+                sessionIds.set(sender, uuid.v1());
+            }
+
+            var ReqSenderID = event.sender.id.toString();
+            var ReqRecipientID = event.recipient.id.toString();
+            var ReqMessageText = text;
+            var ReqTimeStamp;
+            var ReqMessageID;
+
+            if (event.timestamp) {
+                ReqTimeStamp = event.timestamp.toString();
+            }
+
+            if (event.message) {
+                if (event.message.mid) {
+                    ReqMessageID = event.message.mid.toString();
+                }
+            }
+
+            if (event.postback && event.postback.payload && event.postback.payload.indexOf("RetryAuthCode|") > 0) {
+                var authCode = event.postback.payload.split("|")[1];
+                var paramArr = { authCodeParam: authCode, senderParam: sender, userIdParam: "" };
+
+                //getvzUserID(authCode, userCoversationArr, function (str) { getvzUserIDCallback(str, paramArr, userCoversationArr) });
+
+
+            } else {
+
+                // api.ai request moved to function
+                callapiai(text, sender, sessionIds, userCoversationArr);
+            }
+        } else if (event.account_linking) {
+            //logger.debug("event account_linking content :- " + JSON.stringify(event.account_linking));
+            if (event.account_linking == undefined) {
+                //  logger.debug("Account Linking null - 2");
+            }
+            else if (event.account_linking.status === "linked") {
+                //logger.debug("Account Linking convert: Auth Code" + JSON.stringify(event.account_linking.authorization_code, null, 2));
+                logger.debug("Account Linking convert: Status " + JSON.stringify(event.account_linking.status, null, 2));
+                var authCode = event.account_linking.authorization_code;
+
+                //delete event.account_linking;
+                var paramArr = { authCodeParam: authCode, senderParam: sender, userIdParam: "" };
+                userCoversationArr.ufdreqdatetime = getDateTime();
+                getvzUserID(authCode, userCoversationArr, function (str) { getvzUserIDCallback(str, paramArr, userCoversationArr) });
+
+            } else if (event.account_linking.status === "unlinked") {
+                //Place holder code to unlink.
+                logger.debug("Account unlinked");
+                userCoversationArr.ufdreqdatetime = getDateTime();
+                //DeleteAuthProfile(sender, userCoversationArr, function (str) { DeleteAuthProfileCallback(str, sender, userCoversationArr) });
+            }
+        }
+    });
+}
+
+
+function callapiai(msgtext, sender, sessionIds, userCoversationArr) {
+
+    logger.debug("api ai start");
+
+    var payloadIntent = '';
+    var response = '';
+    var strIntent = '';
+    var actionname = '';
+    var result = '';
+
+    try {
+
+        logger.debug("apiai Call text " + msgtext);
+        logger.debug("apiai sender ID " + sender);
+
+        if (msgtext.indexOf('|Payload:recorddetails|') > -1) {
+
+            // logger.debug('inside payload intent with record details');
+
+            userCoversationArr.apireqdatetime = getDateTime();
+
+            var formattedResponse = NLPresponseFormatter("custom", msgtext);
+
+            //logger.debug('payloadmessage::::' + JSONbig.stringify(formattedResponse));
+
+            strIntent = formattedResponse.formattedResponse.parameters.Intent;
+
+            //logger.debug('insidepayloadstrIntent::::' + JSONbig.stringify(strIntent));
+
+            response = result;
+            actionname = strIntent;
+
+            userCoversationArr.action = actionname;
+            userCoversationArr.intent = actionname;
+            userCoversationArr.apiresdatetime = getDateTime();
+            userCoversationArr.apiTimeTaken = getsecondstaken('apiai', userCoversationArr.apireqdatetime, userCoversationArr.apiresdatetime);
+            userCoversationArr.apiaireq = 'passed';
+            printChatHistory(userCoversationArr);
+
+            Findswitchcase(formattedResponse, actionname, strIntent, sender, userCoversationArr, "");
+
+
+
+        }
+        else if (msgtext.indexOf('|Payload|') > -1) {
+
+            //logger.debug('inside payload intent with other payloads');
+
+            userCoversationArr.apireqdatetime = getDateTime();
+
+            var formattedResponse = NLPresponseFormatter("custom", msgtext);
+            //logger.debug('payloadmessage::::' + JSONbig.stringify(formattedResponse));
+
+            strIntent = formattedResponse.formattedResponse.parameters.Intent;
+
+            //logger.debug('insidepayloadstrIntent::::' + JSONbig.stringify(strIntent));
+
+            response = result;
+            actionname = strIntent;
+
+            userCoversationArr.action = actionname;
+            userCoversationArr.intent = actionname;
+            userCoversationArr.apiresdatetime = getDateTime();
+            userCoversationArr.apiTimeTaken = getsecondstaken('apiai', userCoversationArr.apireqdatetime, userCoversationArr.apiresdatetime);
+            userCoversationArr.apiaireq = 'passed';
+            printChatHistory(userCoversationArr);
+
+
+            Findswitchcase(formattedResponse, actionname, strIntent, sender, userCoversationArr, "");
+        }
+        else {
+
+            logger.debug('Getting ready to send information to api.ai');
+
+            userCoversationArr.apireqdatetime = getDateTime();
+
+            var apiaiRequest = apiAiService.textProxyRequest(msgtext, { sessionId: sessionIds.get(sender) });
+
+            apiaiRequest.on('response', function (response) {
+
+                if (isDefined(response.result)) {
+
+                    logger.debug("Api.Ai Full Response " + JSON.stringify(response));
+
+                    var responseText = response.result.fulfillment.speech;
+                    var responseData = response.result.fulfillment.data;
+                    var action = response.result.action;
+
+                    var intent = response.result.metadata.intentName;
+                    var Finished_Status = response.result.actionIncomplete;
+
+                    //logger.debug("Finished_Status " + Finished_Status);
+                    logger.debug('responseText  : - ' + responseText);
+                    //logger.debug('responseData  : - ' + responseData);
+                    logger.debug('action : - ' + action);
+                    logger.debug('intent : - ' + intent);
+
+                    var logdatetime = getDateTime();
+
+                    userCoversationArr.action = action;
+                    userCoversationArr.intent = intent;
+
+                    userCoversationArr.apiresdatetime = getDateTime();
+                    userCoversationArr.apiTimeTaken = getsecondstaken('apiai', userCoversationArr.apireqdatetime, userCoversationArr.apiresdatetime);
+                    userCoversationArr.apiaireq = 'passed';
+                    printChatHistory(userCoversationArr);
+
+                    var strNLP = config.NLP;
+
+                    var formattedResponse = NLPresponseFormatter(strNLP, response);
+
+                    // see if the intent is not finished play the prompt of API.ai or fall back messages
+                    if (Finished_Status == true || intent == "Default Fallback Intent") {
+                        sendFBMessage(sender, { text: responseText }, userCoversationArr);
+                    }
+                    else //if the intent is complete do action
+                    {
+                        logger.debug("----->>>>>>>>>>>> INTENT SELECTION <<<<<<<<<<<------");
+
+                        // Methods to be called based on action
+                        //Findswitchcase(response, action, intent, sender, userCoversationArr, "");
+
+                        Findswitchcase(formattedResponse, action, intent, sender, userCoversationArr, responseText);
+                    }
+                }
+            });
+
+            apiaiRequest.on('error', function (error) {
+                logger.debug("Error on sending request to api.ai " + error)
+                userCoversationArr.apiaireq = 'error';
+                printChatHistory(userCoversationArr);
+
+            });
+
+
+            apiaiRequest.end2();
+
+        }
+    }
+    catch (apiaierror) {
+        logger.error("apiai Error in sending message to apiai " + apiaierror)
+    }
+
+    logger.debug("apiai end");
+}
+
+function getsecondstaken(whatreq, fromdate, todate) {
+    var retsecondsTook;
+    try {
+        var reqDate = new Date(fromdate);
+        var resDate = new Date(todate);
+
+        var differenceTravel = resDate.getTime() - reqDate.getTime();
+
+        retsecondsTook = Math.floor((differenceTravel) / (1000));
+
+        logger.debug("Total seconds Taken for " + whatreq + " is " + retsecondsTook);
+
+        retsecondsTook = retsecondsTook.toString();
+    }
+    catch (dateDiffexp) {
+        logger.debug("Exception while getting the time taken between two dates : " + dateDiffexp)
+    }
+
+    return retsecondsTook;
+}
+function Findswitchcase(response, responseText, strIntent, sender, userCoversationArr, apiairesp) {
+
+    logger.debug("----->>>>>>>>>>>> INTENT SELECTION <<<<<<<<<<<------");
+    //logger.debug("Findswitchcase payload " + JSONbig.stringify(response));
+    //logger.debug("Findswitchcase apiairesp " + JSONbig.stringify(apiairesp));
+    //logger.debug("Selected_action : " + responseText);
+    // Methods to be called based on action 
+    switch (responseText) {
+
+        case "TheaterSearch":
+            logger.debug("----->>>>>>>>>>>> INSIDE MoreOptions <<<<<<<<<<<------");
+            userCoversationArr.ufdreqdatetime = '';
+            userCoversationArr.ufdresdatetime = ''
+            userCoversationArr.ufdTimeTaken = ''
+            userCoversationArr.ufdreq = 'NA'
+            
+            break;
+
+        default:
+            logger.debug("----->>>>>>>>>>>> INSIDE default <<<<<<<<<<<------");
+
+            if ((apiairesp == undefined) || (apiairesp == ''))
+                        
+
+            break;
+    }
+}
+function NLPresponseFormatter(NLP, response) {
+    var formattedResponse = {};
+    if (NLP == "apiai") {
+        //convert api resp to below format
+        formattedResponse = { "formattedResponse": { "parameters": response.result.parameters } };
+    }
+    else if (NLP == "watson") {
+        //convert Watson  resp to below format
+        formattedResponse = { "formattedResponse": { "parameters": { "Channel": "HBO", "ChannelGenre": "", "date": "", "Genre": "", "Programs": "" } } };
+    }
+    else if (NLP == "custom") { //payload concept
+
+        //convert payload resp to below format
+
+        //logger.debug("custom payload conversion" + response);
+
+        var result = { parameters: {} };
+        {
+            response.split('|').forEach(function (x) {
+                var arr = x.split(':');
+                arr[1] && (result.parameters[arr[0]] = arr[1]);
+                arr[1] && ("{" + arr[0].trim() + ":" + arr[1].trim() + "}");
+            });
+
+            //  logger.debug('strPayloadresult : ' + JSONbig.stringify(result));
+        }
+
+        formattedResponse = { "formattedResponse": result };
+    }
+
+    return formattedResponse;
+}
+function getsecondstaken(whatreq, fromdate, todate) {
+    var retsecondsTook;
+    try {
+        var reqDate = new Date(fromdate);
+        var resDate = new Date(todate);
+
+        var differenceTravel = resDate.getTime() - reqDate.getTime();
+
+        retsecondsTook = Math.floor((differenceTravel) / (1000));
+
+        logger.debug("Total seconds Taken for " + whatreq + " is " + retsecondsTook);
+
+        retsecondsTook = retsecondsTook.toString();
+    }
+    catch (dateDiffexp) {
+        logger.debug("Exception while getting the time taken between two dates : " + dateDiffexp)
+    }
+
+    return retsecondsTook;
+}
